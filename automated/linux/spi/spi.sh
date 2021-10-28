ea2fcc782449c65265f35bb1fad1e5b0a7e649ff
seoji <seoji.kim@lgepartner.com>
2021-10-28 18:08:57 +0900

    linux: spi: add params for selecting spidev
    This patch add params on test definition file to select spidev.
    
    Signed-off-by: seoji <seoji.kim@lgepartner.com>
    Change-Id: Ib8cc17f4e650a7101207049ac325ef1987b67923

--- automated/linux/spi/spi.sh
+++ automated/linux/spi/spi.sh
@@ -7,14 +7,18 @@ LOGFILE="${OUTPUT}/spi-stdout.txt"
 TEST_LOG="spi-test-log.txt"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
+SPI_ADDR="10980000"
+SPI_DEV="spidev8.0"
 
 usage() {
     echo "Usage: $0" 1>&2
     exit 1
 }
 
-while getopts "h" o; do
+while getopts "a:d:h" o; do
   case "$o" in
+    a) SPI_ADDR="${OPTARG}" ;;
+    d) SPI_DEV="${OPTARG}" ;;
     h|*) usage ;;
   esac
 done
@@ -22,9 +26,8 @@ done
 # TC Auto9-52181
 test_spi_master_mode1() {
     info_msg "Running Verify Master Mode Transfer over SPI Test1..."
-
-    echo 0 > /sys/devices/platform/10900000.spi/spi_dbg
-    spidev_test -D /dev/spidev1.0 -v -s 25000000
+    echo 0 > /sys/devices/platform/"$SPI_ADDR.spi"/spi_dbg
+    spidev_test -D /dev/"$SPI_DEV" -v -s 25000000
     check_return "verify-spi-master-mode-test1"
 }
 
@@ -32,8 +35,8 @@ test_spi_master_mode1() {
 test_spi_master_mode2() {
     info_msg "Running Verify Master Mode Transfer over SPI Test2..."
 
-    echo 0 > /sys/devices/platform/10900000.spi/spi_dbg
-    spidev_test -D /dev/spidev1.0 -s 25000000 -S 4096 -I 100 | tee $LOGFILE
+    echo 0 > /sys/devices/platform/"$SPI_ADDR.spi"/spi_dbg
+    spidev_test -D /dev/"$SPI_DEV" -s 25000000 -S 4096 -I 100 | tee $LOGFILE
     diff $LOGFILE $TEST_LOG
     check_return "verify-spi-master-mode-test2"
 }
